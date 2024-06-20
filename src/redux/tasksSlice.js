3fb1a91e393001ac1876574ff2d8ada5baa2a96b
SergeyMaitamal7 <96728835+SergeyMaitamal7@users.noreply.github.com>
2024-06-20 15:08:38 +0300

    start2

--- src/redux/tasksSlice.js
+++ src/redux/tasksSlice.js
@@ -2,13 +2,7 @@ import { createSlice } from '@reduxjs/toolkit';
 import { nanoid } from 'nanoid';
 
 const taskInitialState = {
-  items: [
-    { id: 0, text: 'Learn HTML and CSS', completed: true },
-    { id: 1, text: 'Get good at JavaScript', completed: true },
-    { id: 2, text: 'Master React', completed: false },
-    { id: 3, text: 'Discover Redux', completed: false },
-    { id: 4, text: 'Build amazing apps', completed: false },
-  ],
+  items: [],
   isLoading: false,
   error: null,
 };
